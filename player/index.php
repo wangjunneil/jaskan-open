e32ccab0cd83ac1161be818a4ca61f884b2a0211
哲剑 <zhejian.wj@alipay.com>
2023-02-03 14:25:33 +0800

    新播放器修改

--- player/index.php
+++ player/index.php
@@ -20,7 +20,7 @@
     <style>
         /*隐藏页面全屏按钮，隐藏加载动画，隐藏视频信息屏蔽词汇，隐藏弹幕规则*/
         .yzmplayer-info-panel-item-title-amount ,#loading-box,.yzmplayer-full .yzmplayer-full-in-icon,#link3-error,.dmrules{
-            display: block !important;
+            display: none !important;
         }
         .yzmplayer-full-icon span,
         .yzmplayer-fulloff-icon span {
