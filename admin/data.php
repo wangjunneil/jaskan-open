381443130856234d06f5c7b8f9a6590aaceec0d3
Aaron Wong <wangjunneil@gmail.com>
2023-02-07 20:43:26 +0800

    fix

--- admin/data.php
+++ admin/data.php
@@ -9,11 +9,10 @@
   'pbgjz' => '草,操,妈,逼,滚,网址,网站,支付宝,企,关注,wx,微信,qq,QQ',
   'ads' => 
   array (
-    'state' => 'on',
     'pause' => 
     array (
-      'pic' => '',
-      'link' => '#',
+      'pic' => 'https://fla.cdn.bosyun.com/upload/vod/20220303-1/08747783aae4a0cfc026dbbcb60003f4.jpg',
+      'link' => 'https://www.baidu.com',
     ),
   ),
 );
